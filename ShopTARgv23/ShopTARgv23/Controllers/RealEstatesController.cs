c02ac01a5ecc47c50c41687d72c9ef7ab4bb63b5
Ingvar Derešivski <inde999@gmail.com>
2024-10-07 15:46:38 +0300

    RealEstateIndex

---
+++ ShopTARgv23/ShopTARgv23/Controllers/RealEstatesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARgv23.Data;
+using ShopTARgv23.Models.RealEstates;
+
+namespace ShopTARgv23.Controllers
+{
+    public class RealEstatesController : Controller
+    {
+        private readonly ShopTARgv23Context _context;
+
+        public RealEstatesController
+            (
+                ShopTARgv23Context context
+            )
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.RealEstates
+                .Select(x => new RealEstatesIndexViewModel
+                {
+                    Id = x.Id,
+                    Location = x.Location,
+                    Size = x.Size,
+                    RoomNumber = x.RoomNumber,
+                    BuildingType = x.BuildingType,
+                });
+
+            return View(result);
+        }
+    }
+}
