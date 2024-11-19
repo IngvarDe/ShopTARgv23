18a3303dc424f5da88c2a703b31df3c13492a8b4
Ingvar Derešivski <inde999@gmail.com>
2024-11-19 11:32:15 +0200

    CoctailServices

---
+++ ShopTARgv23/ShopTARgv23.Core/ServiceInterface/ICocktailServices.cs
@@ -0,0 +1,11 @@
+﻿
+
+using ShopTARgv23.Core.Dto.CocktailDto;
+
+namespace ShopTARgv23.Core.ServiceInterface
+{
+    public interface ICocktailServices
+    {
+        Task<CocktailResultDto> GetCocktails(CocktailResultDto dto);
+    }
+}
