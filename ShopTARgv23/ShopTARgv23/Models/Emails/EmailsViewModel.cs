e16eae63434c289179df6839d9d06fa100c82c85
Ingvar Derešivski <inde999@gmail.com>
2024-11-25 15:04:16 +0200

    EmailSend

---
+++ ShopTARgv23/ShopTARgv23/Models/Emails/EmailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopTARgv23.Models.Emails
+{
+    public class EmailsViewModel
+    {
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
