46531ef9cafb10ff66cec197e3fe8ae2d2de995e
Ingvar Derešivski <inde999@gmail.com>
2024-12-16 16:43:24 +0200

    EmailResponseInProgress

--- ShopTARgv23/ShopTARgv23.Core/ServiceInterface/IEmailsServices.cs
+++ ShopTARgv23/ShopTARgv23.Core/ServiceInterface/IEmailsServices.cs
@@ -1,14 +1,11 @@
 ﻿using ShopTARgv23.Core.Dto;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ShopTARgv23.Core.ServiceInterface
 {
     public interface IEmailsServices
     {
         void SendEmail(EmailDto dto);
+        void SendEmailToken(EmailTokenDto dto, string token);
     }
 }
